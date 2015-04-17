741aa498a85370e1f45d4f805794b41838280fa6
a4425421 <aa4425421@hotmail.com>
2015-04-17 16:01:11 +0800

    1
    1

--- src/com/example/im_test/MainActivity.java
+++ src/com/example/im_test/MainActivity.java
@@ -11,7 +11,8 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		System.out.println("ddddddddddddddddd");
+		System.out.println("ddddddddddddddddd2");
 	}
 
 	@Override
